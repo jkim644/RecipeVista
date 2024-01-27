c4d720d6384117ee738367b9787c9d920ce5e8c4
hmkuklinski <hmkuklinski@gmail.com>
2024-01-27 06:34:09 -0500

    added the randomized entree option -> will update title, image, provide ingredients (can click and add to shopping list), along with a love it button (locks in entree for spin)
    still need to do recipe pop up information w directions and the two sides

--- javascript.js
+++ javascript.js
@@ -2,47 +2,39 @@
 const lock1Btn = document.getElementById('btn-1');
 const lock2Btn = document.getElementById('btn-2');
 const lock3Btn = document.getElementById('btn-3');
-
 const spinBtn= document.querySelector('.btn-spin');
-
 const allergyBtn= document.querySelector('.btn-allergy');
 const veganBtn = document.querySelector('.btn-vegan');
 const vegetarianBtn = document.querySelector('.btn-vegetarian');
 const noDietBtn = document.querySelector('.btn-none');
+const closeBtn= document.querySelector('.btn-close');
+const recipeContentBox = document.querySelector('.recipe-content');
 
+
+//allows users to lock in their desired item so not lost in respin
 let item1Lock= false;
 let item2Lock= false;
 let item3Lock= false;
 
-//possible variables:
+//possible variables for diet/allergies (user selection options):
 let isVegetarian = false;
 let isVegan = false;
 let noDiet = false;
 let hasAllergy= false;
 let allergy =[];
-const validAllergies = ['milk', 'eggs', 'fish', 'shellfish', 'crab', 'lobster', 'shrimp', 'citrus', 'lemon', 'lime', 'orange', 'almond', 'walnuts', 'pecans', 'nuts', 'tree nuts', 'peanuts', 'wheat', 'soybeans', 'sesame'];
-
-//get information from allergy text box
-
-//will select the new entree/side dish/drink option
-function spin(){};
-
-//randomly select a
-
-//function that will change the images to simulate a spin
-function randomizeImg(){};
 
 //locks in the option
-function lockIn(){};
+function lockEntree(){
+    item1Lock = true;
+}
+function lockSide(){
+    item2Lock = true;
+}
+function lockSide2(){
+    item3Lock=true;
+}
+lock1Btn.addEventListener('click', lockEntree);
 
-//adds ingredients to shopping list
-function addToShoppingList(){};
-
-//remove ingredients from shopping list
-function removeFromList(){};
-
-//shows recipe
-function showRecipe(){};
 
 //adds allergy to list --> changes hasAllergy to true
 function enterAllergy(){
@@ -51,17 +43,10 @@ function enterAllergy(){
 
     if (allergen.trim() !== ''){
         allergyBox.value=''; //clears input box
-        for (let i=0;i<validAllergies.length;i++){
-            if (validAllergies[i] === allergen.trim()){
-                addToAllergyList(allergen);
-                alert("Allergy Added");
-                hasAllergy = true;
-                break;
-            }
-        }
-        if (hasAllergy === false){
-            alert('Allergy not found in allergens list. Please try again');
-        }
+        addToAllergyList(allergen);
+        alert("Allergy Added");
+        console.log("Allergen: " + allergy);
+        hasAllergy = true;
     }
     else{ //doesn't type anything, but hits button
         hasAllergy =false;
@@ -101,3 +86,113 @@ allergyBtn.addEventListener('click', enterAllergy);
 veganBtn.addEventListener('click', selectedVegan);
 vegetarianBtn.addEventListener('click', selectedVegetarian);
 noDietBtn.addEventListener('click', selectedNone);
+
+//shopping list:
+const shoppingList= [];
+let validEntrees =[];
+//get an entree --> valid categories depend on dietary restriction selection:
+if (isVegetarian){
+    validEntrees = ["Vegetarian"]
+}
+else if(isVegan){
+    validEntrees = ["Vegan"]
+}
+else{
+    validEntrees= ["Beef", "Chicken", "Goat", "Lamb", "Pasta", "Pork", "Seafood"]; //will not include vegan/vegetarian options. only if selected
+}
+
+let containsAllergen= false;
+async function getEntree(item1Lock){
+    if (item1Lock){
+        return;
+    }
+    while(true){
+        let containsAllergen= false; //reset
+        try{
+            const response= await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+            const data= await response.json(); //an object that has our meal data from api
+
+            const category= data.meals[0].strCategory;  //access the category type
+            const youtube = data.meals[0].strYoutube; //gives youtube link
+            const recipeName= data.meals[0].strMeal;
+            const previewImgSrc = data.meals[0].strMealThumb; //gives us img link
+            const instructions= data.meals[0].strInstructions; //instructions
+            const recipeLinkSrc= data.meals[0].strSource; //view original recipe link
+            let ingredientsList= []; //to add all ingredients to check for allergen
+            
+            //add all of the recipe's ingredients to ingredients list (don't add empty entries!)
+            for(let i=1;i<=20;i++){
+                const ingredient = data.meals[0][`strIngredient${i}`];
+                if (ingredient !== null && ingredient !=='') {
+                    ingredientsList.push(ingredient);
+                } else {
+                    // Break the loop if there are no more ingredients
+                    break;
+                }
+            }
+
+            //check if ingredient is an allergen (entered by user)
+            if (ingredientsList.includes(allergy)){
+                containsAllergen= true;
+            }
+
+            //to provide valid recipes that follow the category and do not include allergens:
+            if (validEntrees.includes(category) && containsAllergen === false){
+                //change recipe name:
+                document.getElementById('row-1-title').textContent = recipeName;
+
+                //change recipe picture:
+                document.querySelector('.img-preview img').src= previewImgSrc;
+                
+                //add ingredients to li: (with add button)
+                console.log(ingredientsList);
+                const ulElement = document.querySelector('ul'); // Assuming you have a <ul> element in your HTML
+
+                //cycle through each ingredient and add to unordered list (with added classes to get proper formatting)
+                ingredientsList.forEach(ingredient => {
+                    const liElement = document.createElement('li');
+                    liElement.textContent = ingredient;
+
+                    // Apply styles to make li look like a button
+                    liElement.classList.add('btn-ingredient', 'recipe-category');
+
+                    // Add click event listener --> user wants to add to shopping cart
+                    liElement.addEventListener('click', () => {
+                        addToShoppingList(ingredient);
+                        alert(`Added ${ingredient} to your shopping list!`);
+                    });
+
+                    // Append the li element to your existing ul element
+                    ulElement.appendChild(liElement);
+                });
+                
+
+                break;
+            }
+            
+    
+    
+        }catch(error){
+            console.error('Error fetching random meal: ', error);
+        }
+    }
+}
+
+spinBtn.addEventListener('click', selectMenu);
+
+function selectMenu(){
+    getEntree(item1Lock);
+    //getSide(item2Lock);
+    //getSide2(item3Lock);
+}
+function addToShoppingList(ingredient){
+    shoppingList.push(ingredient);
+}
+
+function showDirections(){
+    document.querySelector('.recipe-details').style.display="block";
+}
+
+function hideDirections(){
+    document.querySelector('.recipe-details').style.display="none";
+}
